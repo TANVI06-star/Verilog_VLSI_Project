06f749d5e79eb2d53a53e65e948a1969d041c1d4
Tanvi Nariya <nariyatanvi11@gmail.com>
2026-09-03 10:03:55 +0530

    UPDATED CODES

--- FSM/traffic_light/trafficlight_fsm.v
+++ FSM/traffic_light/trafficlight_fsm.v
@@ -5,7 +5,7 @@
 //              Traffic light controller using FSM concept.(3 PROCESS)
 //---------------------------------------------------------------
 
-module tl(input clk,input reset,output reg Y,output reg [2:0]SIGNAL);
+module tl(input clk,input reset,output reg [2:0]SIGNAL);
 
 //STATE DECLARATION AND COUTER
 reg [1:0]state,next_state;
@@ -24,10 +24,29 @@ begin
 if(reset)
 begin
 state <= RED;
-count <= 0;
+count <= 3'b0;
 end
-else
+else 
+begin
 state <= next_state;
+
+if ((state == RED) && (count == 9))
+count <= 1'b0;
+else
+count <= count + 1'b1;
+
+if ((state == YELLOW) && (count == 3))
+count <= 1'b0;
+else
+count <= count + 1'b1;
+
+if ((state == GREEN) && (count == 9))
+count <= 1'b0;
+else
+count <= count + 1'b1;
+
+end
+
 end
 
 //NEXT STATE LOGIC
@@ -36,43 +55,35 @@ begin
 next_state = RED;
 case(state)
 RED : begin
-        if(count < 9)begin
+        if(count <= 9)begin
                   next_state = RED;
                   SIGNAL = 3'b100;
-                count = count + 1;
                 
         end
         else begin
               next_state = YELLOW;
               SIGNAL = 3'b010;
-                count = 0;
         end
 
         end
 YELLOW : begin
-        if(count < 3)begin
+        if(count <= 3)begin
                   next_state = YELLOW;
                   SIGNAL = 3'b010;
-                count = count + 1;
-                
         end
         else begin
               next_state = GREEN;
               SIGNAL = 3'b001;
-                count = 0;
         end
 end
 GREEN : begin
-        if(count < 9)begin
+        if(count <= 9)begin
                   next_state = GREEN;
                   SIGNAL = 3'b001;
-                count = count + 1;
-                
         end
         else begin
               next_state = RED;
               SIGNAL = 3'b100;
-                count = 0;
         end
 end
 default : begin
@@ -82,14 +93,6 @@ end
 endcase
 end
 
-//OUTPUT LOGIC
-always @(*)
-begin
-        if(state == GREEN && count == 9)
-        Y = 1'b1;
-        else
-        Y = 1'b0;
-end
 
 endmodule
 
@@ -98,16 +101,17 @@ endmodule
 `timescale 1ns/1ps
 module tb;
 reg clk,reset;
-wire Y;
 wire [2:0]signal;
+integer error;
 
-tl dut(.clk(clk),.reset(reset),.Y(Y),.SIGNAL(signal));
+//driver
+tl dut(.clk(clk),.reset(reset),.SIGNAL(signal));
 
 //clock generation
 initial
 begin
 clk = 0;
-forever #5 clk = ~clk;
+forever #1 clk = ~clk;
 end
 
 
@@ -116,15 +120,34 @@ begin
 $dumpfile("traffic_light.vcd");
 $dumpvars(0,tb);
 
-$monitor("TIME = %0t | clk = %b | Y =%b | SIGNAL = %b",$time,clk,Y,signal);
+//monitoring
+$monitor("TIME = %0t | clk = %b | SIGNAL = %b",$time,clk,signal);
 
 //INITIALIZING CLOCK
-reset=1;
-#12 reset=0;
-
+reset=1;error = 0;
+#10 reset =0;
 #100 $finish;
 
+// DISPLAYING THE RESULT THE OF SCORECARD
+if(error == 0)
+$display("SIMULATION IS PASS.");
+else
+$display("SIMULATION IS FAILS.");
+
 end
+
+//SCORECARD
+always @(*)
+begin
+        if(((|signal) != 1 ))
+        begin
+                $display("FAIL");
+                error = error + 1;
+        end
+        else
+        $display("PASS.");
+end
+
 
 endmodule
 
